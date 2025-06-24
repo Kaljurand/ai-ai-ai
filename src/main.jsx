6304108171103392fba4b5710f16df0423919307
Kaarel Kaljurand <kaljurand@gmail.com>
2025-06-24 04:36:23 +0200

    Add playground icon and adjust UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: { main: '#2e7d32' }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
